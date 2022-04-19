eda89d57914ff25e3f4dbea3537f55f1bf3e3025
Willis1118 <nm3607@nyu.edu>
2022-04-18 21:35:09 -0400

    client

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from './App';
+
+//entire frontend app rendered here
+ReactDOM.render(<App/>, document.getElementById('root'))
